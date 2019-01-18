ba7142bd90ef550761e2b95f9d43c7a3a5f0db5c
louisah22 <46412571+louisah22@users.noreply.github.com>
2019-01-18 00:17:49 -0800

    Update test-sketch.js

--- test-sketch.js
+++ test-sketch.js
@@ -12,24 +12,22 @@ function draw() {
 	textSize(40);
 	text("Vancouver", 40, 75); 
 	//shapes
-	var x = 0
-	var y = 0
 	strokeWeight(1);
 	fill(65, 193, 68);
 	beginShape();
-	vertex(50+x, 200+y); //left top
-	vertex(150+x, 450+y); //left bottom
-	vertex(325+x, 490+y); // bottom middle
-	vertex(550+x, 450+y); //right bottom
-	vertex(550+x, 200+y); //right top
-	vertex(400+x, 200+y); // triangle right
-	vertex(300+x, 100+y); //triangle point
-	vertex(275+x, 200+y); //triangle left
-	vertex(50+x, 200+y); //left top
+	vertex(50, 200); //left top
+	vertex(150, 450); //left bottom
+	vertex(325, 490); // bottom middle
+	vertex(550, 450); //right bottom
+	vertex(550, 200); //right top
+	vertex(400, 200); // triangle right
+	vertex(300, 100); //triangle point
+	vertex(275, 200); //triangle left
+	vertex(50, 200); //left top
 	endShape();
 	//circle 1
 	fill(244, 122, 66);
-	ellipse(200+x, 250+y, 50, 50);
+	ellipse(200, 250, 50, 50);
 	//circle 2
   fill(244, 122, 66);
 	ellipse(405+x, 405+y, 71, 71);
